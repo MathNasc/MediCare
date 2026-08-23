2b05ef3e3d6175d385985893613e97bd12f18e1e
Matheus do Nascimento Araujo <49731091+MathNasc@users.noreply.github.com>
2026-08-23 16:00:52 -0300

    v1

--- fix.js
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-let code = fs.readFileSync('src/screens/ProfileScreen.jsx', 'utf8');
-code = code.replace(/document.body\n  \);\nimport/, "document.body\n  );\n}\n\nimport");
-fs.writeFileSync('src/screens/ProfileScreen.jsx', code);
--- test-webpush.ts
+++
@@ -1,7 +0,0 @@
-import webpush from 'npm:web-push@3.6.7';
-try {
-  webpush.setVapidDetails('mailto:a@a.com', undefined as any, undefined as any);
-  console.log('Did not throw');
-} catch (e) {
-  console.log('Threw error:', e.message);
-}
